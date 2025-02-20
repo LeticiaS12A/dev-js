b94f0d6a260e1aae6fcf1ed51b1017d80c12c4b6
LeticiaS12A <leticia.s.a.tecns@gmail.com>
2025-02-19 21:59:43 -0300

    Add files via upload

---
+++ js/variable.js
@@ -0,0 +1,18 @@
+// eu sou um comentario
+
+// existe 3 variaveis no js
+// variaveis que podem ser alteradas
+// legado
+var nomeDaVariavel = 'js'
+
+let one = 1
+
+const preco = 10
+
+console.log(preco)
+
+// preco = 15 - nao pode reassinar uma variavel conts
+
+one = 3
+
+console.log(one, 'estou aqui')
